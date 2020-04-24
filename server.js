d1ab837436e5e74ffb6fbec4942f2b3120f80eee
sailyw <1152827086@qq.com>
2020-04-24 23:08:04 +0800

    标题

--- server.js
+++ server.js
@@ -26,12 +26,26 @@ var server = http.createServer(function (request, response) {
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`二哈`);
+    response.write(`
+        <!DOCTYPE html>
+        <head>
+            <link rel="stylesheet" href="/x">
+        </head>
+        <body>
+            <h1>标题</h1>
+            <script src="/y"></script>
+        </body>
+    `);
     response.end();
   } else if (path === "/x") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
     response.write(`body{color: red;}`);
+    response.end();
+  } else if (path === "/y") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/javascript;charset=utf-8");
+    response.write(`console.log("js真香")`);
     response.end();
   } else {
     response.statusCode = 404;
